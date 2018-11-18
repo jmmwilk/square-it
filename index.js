caf565536db45c20a1484a7ede19ae7a1fe261ae
aw2501 <aleksander.wilk@gmail.com>
2018-11-18 18:04:21 +0100

    Drag and drop - work in progress

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let height = Math.floor (Math.random () * 4) + 1;
 let width = Math.floor (Math.random () * 4) + 1;
 let multiplication = height * width;
-
+let x = 1;
+let y = 1;
+let countY = 01
 function start () {
   drawSquaresInGame ();
   drawRectangle ();
+
+  let game = document.getElementById('application');
+  game.ondrop = function (event) {
+    console.log(event);
+  }
+  game.ondragover= function (event) {
+    event.preventDefault();
+  }
 }
 
 function drawSquaresInGame () {
@@ -12,15 +22,29 @@ function drawSquaresInGame () {
   for (i=0; i<100; i++) {
     let game = document.getElementById('game');
 //    square.style.left = 100 + 'px';
-    drawSquares (game, 'white');
+    drawSquare (game, 'white');
   }
+  x = 1;
+  y = 1;
 }
 
-function drawSquares (parent, color) {
+function drawSquare (parent, color) {
   let square = document.createElement("div");
   square.className = 'square';
   square.style.backgroundColor = color
   let addsquare = parent.appendChild(square);
+  square.x = x;
+  square.y = y;
+  if (x == 10) {
+    x = 0;
+  }
+  if(countY == 10) {
+    y = y + 1;
+  }
+  x= x +1;
+  countY= countY + 1;
+  console.log ('x' + square.x);
+  console.log ('y' + square.y);
 }
 
 
@@ -30,19 +54,25 @@ function drawRectangle () {
   rectangle.className = 'rectangle';
   rectangle.style.height = (height * 50 + 2) + 'px';
   rectangle.style.width = (width * 50 + 2) + 'px';
-  console.log(rectangle.style.height);
+  rectangle.draggable = true;
+  rectangle.ondragstart = function(event) {
+    event.dataTransfer.setData("text/plain", event.target.id);
+  }
   document.getElementById('side-bar').appendChild(rectangle);
   drawSquaresInRectangle(rectangle);
 }
 
 function drawSquaresInRectangle (rectangle) {
   for (i=0; i<multiplication; i++) {
-    drawSquares(rectangle, 'green');
+    drawSquare(rectangle, 'green');
   }
 }
 
-
-
+function () {
+   for (i=0; i<100; i++) {
+    if (document.getElementByClassName('square').x == 
+   }
+}
 
 // function drawRectangle () {
 //   let height = Math.floor (Math.random () * 4) + 1;
